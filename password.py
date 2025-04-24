e884f4a98bb4a9673a63d909847eaf923c0aa595
Sruthi20921 <sruthinalla621@gmail.com>
2025-04-24 16:30:32 +0530

    Add files via upload
    Added password generator script

--- password.py
+++ password.py
@@ -1,1 +1,13 @@
+import random
 
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=?><[]{}'
+length = int(input("Enter password length (min 4): "))
+
+if length >= 4:
+    password = ''.join(random.sample(chars, length))
+    print("Password:", password)
+    f = open("simple_passwords.txt", "a")
+    f.write(password + "\n")
+    f.close()
+else:
+    print("Too short!")
